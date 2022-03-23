412b4af75b299aa68bac1abc143cd1c21dfebbd0
ituitis-canb18 <canbatuhan.official@gmail.com>
2022-03-23 13:20:31 +0300

    running tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-for DISTANCE_THRESHOLD in 9 11 13 15
+echo "----------------------------------------------------------"
+echo "Clustering with Integer Linear Programming"
+echo "Unit Distance in the Grid = 1.17 km"
+echo "Model will be run for the following coverage distances;"
+echo "-  7 units  ~  8.5  km"
+echo "-  9 units  ~ 10.5  km"
+echo "- 11 units  ~ 13.0  km"
+echo "- 13 units  ~ 15.25 km"
+echo "- 15 units  ~ 17.5  km"
+echo "----------------------------------------------------------"
+
+
+for DISTANCE_THRESHOLD in 7 9 11 13 15
 do
-    echo "------------------------------------"
-    echo "Optimization is running..."
-    echo "distance_threshold = $DISTANCE_THRESHOLD"
-    echo "------------------------------------"
+    echo "----------------------------------------------------------"
+    echo "Model is running for the coverage distance = $DISTANCE_THRESHOLD units"
+    echo "----------------------------------------------------------"
     python main.py --distance_threshold=$DISTANCE_THRESHOLD
 done
